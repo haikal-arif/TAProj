e8d0b1bbb6acc56099772a3ee10c1b6eb71117d3
haikal arif <haikal20001@outlook.com>
2023-02-21 13:02:35 +0700

    add pwscf result

--- input_pw/Si/Si-gen.sh
+++ input_pw/Si/Si-gen.sh
@@ -1,11 +1,23 @@
+#!/bin/bash
+
+lattice_params=(8.1985 8.4718 8.7450 9.0183 9.2916 9.5649 9.8382 10.1114 10.3847 10.6580 10.9313 11.2046 11.4779 11.7511 12.0244 12.2977)
+FILENAME="PW.in"
+
+for index in "${!lattice_params[@]}"; do
+  DIRNAME="./input_pw/Si/$index/"
+  if [[ ! -d $DIRNAME ]]; then
+    mkdir "$DIRNAME"  
+  fi
+  cat > "$DIRNAME/$FILENAME" << EOF
 &CONTROL
     calculation = "scf"
     max_seconds =  8.64000e+04
     pseudo_dir  = "~/Documents/TA/PWDFT.jl/pseudopotentials/scan_upf/"
+    outdir      = "$DIRNAME"
 /
 
 &SYSTEM
-    celldm(1)   =  10.2481
+    celldm(1)   = ${lattice_params[$index]} 
     ecutwfc     =  5.00000e+01
     ibrav       = 2
     nat         = 2
@@ -37,4 +49,9 @@ Si     28.0855   Si.SCAN.UPF2
 
 ATOMIC_POSITIONS {alat}
 Si      0.000000   0.000000   0.000000
-Si      0.250000   0.250000   0.250000  
+Si      0.250000   0.250000   0.250000 
+
+EOF
+  pw.x < "$DIRNAME/$FILENAME" > "$DIRNAME/PW.out" &
+done
+
