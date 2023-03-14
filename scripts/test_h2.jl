abd12137795be48400acc01646f117628ab33053
Muhammad Haikal A <haikal20001@outlook.com>
2023-03-14 19:50:36 +0700

    add test for hidrogen

---
+++ scripts/test_h2.jl
@@ -0,0 +1,45 @@
+using LinearAlgebra
+using Random
+using Printf
+
+using PWDFT
+
+const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)), "..")
+const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "scan_upf")
+const DIR_STRUCTURES = joinpath(DIR_PWDFT, "structures")
+
+import InteractiveUtils
+InteractiveUtils.versioninfo()
+import Dates
+println("Now = ", Dates.now())
+
+function main(; method="SCF")
+
+    Random.seed!(1234)
+
+    # Atoms
+    atoms = Atoms(xyz_file=joinpath(DIR_STRUCTURES, "H2.xyz"),
+        LatVecs=gen_lattice_sc(16.0))
+
+    # Initialize Hamiltonian
+    pspfiles = [joinpath(DIR_PSP, "H.SCAN.UPF")]
+    ecutwfc = 15.0
+    Ham = Hamiltonian(atoms, pspfiles, ecutwfc, meshk=[10, 10, 10])
+    # println(Ham)
+    if method == "SCF"
+        KS_solve_SCF!(Ham, mix_method="pulay")
+
+    elseif method == "Emin"
+        KS_solve_Emin_PCG!(Ham)
+
+    else
+        error(@sprintf("ERROR: unknown method = %s", method))
+    end
+
+end
+
+# @time main(method="SCF")
+# @time main(method="Emin")
+
+@time main(method="SCF")
+# @time main(method="Emin")
