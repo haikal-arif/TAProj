e7d90c7e3cf816a2c6ee1b3339d3af2d46d45e42
Muhammad Haikal A <haikal20001@outlook.com>
2023-03-29 08:40:11 +0700

    Add new data

--- scripts/generate_points.jl
+++ scripts/generate_points.jl
@@ -11,14 +11,14 @@ const ALL_PADE_PSP = Dict(
     "Ba" => ("Ba-q10.gth", "Ba-q2.gth",),
     "Be" => ("Be-q4.gth", "Be-q2.gth",),
     "Bi" => ("Bi-q5.gth",),
-    "B"  => ("B-q3.gth",),
+    "B" => ("B-q3.gth",),
     "Br" => ("Br-q7.gth",),
     "Ca" => ("Ca-q10.gth", "Ca-q2.gth",),
     "Cd" => ("Cd-q12.gth", "Cd-q2.gth",),
     "Ce" => ("Ce-q12.gth",),
     "Cl" => ("Cl-q7.gth",),
     "Co" => ("Co-q17.gth", "Co-q9.gth",),
-    "C"  => ("C-q4.gth",),
+    "C" => ("C-q4.gth",),
     "Cr" => ("Cr-q14.gth", "Cr-q6.gth",),
     "Cs" => ("Cs-q9.gth", "Cs-q1.gth",),
     "Cu" => ("Cu-q11.gth", "Cu-q19.gth", "Cu-q1.gth",),
@@ -26,7 +26,7 @@ const ALL_PADE_PSP = Dict(
     "Er" => ("Er-q22.gth",),
     "Eu" => ("Eu-q17.gth",),
     "Fe" => ("Fe-q16.gth", "Fe-q8.gth",),
-    "F"  => ("F-q7.gth",),
+    "F" => ("F-q7.gth",),
     "Ga" => ("Ga-q13.gth", "Ga-q3.gth",),
     "Gd" => ("Gd-q18.gth",),
     "Ge" => ("Ge-q4.gth",),
@@ -34,11 +34,11 @@ const ALL_PADE_PSP = Dict(
     "Hf" => ("Hf-q12.gth",),
     "Hg" => ("Hg-q12.gth", "Hg-q2.gth",),
     "Ho" => ("Ho-q21.gth",),
-    "H"  => ("H-q1.gth",),
+    "H" => ("H-q1.gth",),
     "In" => ("In-q13.gth", "In-q3.gth",),
-    "I"  => ("I-q7.gth",),
+    "I" => ("I-q7.gth",),
     "Ir" => ("Ir-q17.gth", "Ir-q9.gth",),
-    "K"  => ("K-q9.gth", "K-q1.gth",),
+    "K" => ("K-q9.gth", "K-q1.gth",),
     "Kr" => ("Kr-q8.gth",),
     "La" => ("La-q11.gth",),
     "Li" => ("Li-q3.gth", "Li-q1.gth",),
@@ -51,14 +51,14 @@ const ALL_PADE_PSP = Dict(
     "Nd" => ("Nd-q14.gth",),
     "Ne" => ("Ne-q8.gth",),
     "Ni" => ("Ni-q18.gth", "Ni-q10.gth",),
-    "N"  => ("N-q5.gth",),
-    "O"  => ("O-q6.gth",),
+    "N" => ("N-q5.gth",),
+    "O" => ("O-q6.gth",),
     "Os" => ("Os-q16.gth", "Os-q8.gth",),
     "Pb" => ("Pb-q4.gth",),
     "Pd" => ("Pd-q18.gth", "Pd-q10.gth",),
     "Pm" => ("Pm-q15.gth",),
     "Po" => ("Po-q6.gth",),
-    "P"  => ("P-q5.gth",),
+    "P" => ("P-q5.gth",),
     "Pr" => ("Pr-q13.gth",),
     "Pt" => ("Pt-q18.gth", "Pt-q10.gth",),
     "Rb" => ("Rb-q9.gth", "Rb-q1.gth",),
@@ -72,7 +72,7 @@ const ALL_PADE_PSP = Dict(
     "Si" => ("Si-q4.gth",),
     "Sm" => ("Sm-q16.gth",),
     "Sn" => ("Sn-q4.gth",),
-    "S"  => ("S-q6.gth",),
+    "S" => ("S-q6.gth",),
     "Sr" => ("Sr-q10.gth", "Sr-q2.gth",),
     "Ta" => ("Ta-q13.gth", "Ta-q5.gth",),
     "Tb" => ("Tb-q19.gth",),
@@ -81,64 +81,78 @@ const ALL_PADE_PSP = Dict(
     "Ti" => ("Ti-q12.gth", "Ti-q4.gth",),
     "Tl" => ("Tl-q13.gth", "Tl-q3.gth",),
     "Tm" => ("Tm-q23.gth",),
-    "V"  => ("V-q13.gth", "V-q5.gth",),
-    "W"  => ("W-q14.gth", "W-q6.gth",),
+    "V" => ("V-q13.gth", "V-q5.gth",),
+    "W" => ("W-q14.gth", "W-q6.gth",),
     "Xe" => ("Xe-q8.gth",),
     "Yb" => ("Yb-q24.gth",),
-    "Y"  => ("Y-q11.gth", "Y-q3.gth",),
+    "Y" => ("Y-q11.gth", "Y-q3.gth",),
     "Zn" => ("Zn-q12.gth", "Zn-q20.gth", "Zn-q2.gth",),
     "Zr" => ("Zr-q12.gth", "Zr-q4.gth",),
 )
 
-const DIR_PWDFT = joinpath(dirname(pathof(PWDFT)),"..")
+ALL_SCAN_PSP = Dict(
+    "Al" => "Al.SCAN.UPF2",
+    "B" => "B.SCAN.UPF2",
+    "Be" => "Be.SCAN.UPF2",
+    "C" => "C.SCAN.UPF2",
+    "Cl" => "Cl.SCAN.UPF2",
+    "F" => "F.SCAN.UPF2",
+    "Ge" => "Ge.SCAN.UPF2",
+    "H" => "H.SCAN.UPF2",
+    "He" => "He.SCAN.UPF2",
+    "Li" => "Li.SCAN.UPF2",
+    "N" => "N.SCAN.UPF2",
+    "Ne" => "Ne.SCAN.UPF2",
+    "O" => "O.SCAN.UPF2",
+    "P" => "P.SCAN.UPF2",
+    "S" => "S.SCAN.UPF2",
+    "Si" => "Si.SCAN.UPF2",)
+
+const DIR_PWDFT = joinpath("C:/Users/mhaikala/Documents/Kuliah/PWDFT.jl/")
 const DIR_PSP = joinpath(DIR_PWDFT, "pseudopotentials", "pade_gth")
+const DIR_PSP_SCAN = joinpath(DIR_PWDFT, "pseudopotentials", "scan_upf")
 
 
 function main()
     lc20 = createLC20()
-    
-    
-    Threads.@threads for atom_recipes in lc20
-        
-        
+
+
+    for atom_recipes in lc20
+
+
         molecule = join(atom_recipes.atom_names)
-        
+
         if molecule ∉ ARGS
             continue
         end
 
-        low_bound = atom_recipes.lattice_param * 0.7
-        upper_bound = atom_recipes.lattice_param * 1.3
-        num_points = 15
+        low_bound = atom_recipes.lattice_param * 0.8
+        upper_bound = atom_recipes.lattice_param * 1.2
+        num_points = 16
         latvecs_vals = range(low_bound, upper_bound, num_points)
-        ecutwfc = 40.0
-        
-        
-        
-        open("result/$molecule.dat", "a") do file
-        line = "================$molecule================\n"
-        print(line)
-        write(file, line)
-        flush(file)
-            
+        ecutwfc = 50.0
 
-        for latvecs_point in latvecs_vals
-            atom_recipes.lattice_param = latvecs_point*ANG2BOHR
-            crystal_structure = build(atom_recipes)
-            pspfiles = map(key -> joinpath(DIR_PSP, ALL_PADE_PSP[strip(key)][1]), atom_recipes.atom_names)
-            ham = Hamiltonian(crystal_structure, pspfiles, ecutwfc, meshk=[10, 10, 10])
-            KS_solve_Emin_PCG!(ham, verbose=false, )
-            total_energy = sum(ham.energies)
-            line = "$latvecs_point\t$total_energy\n"
+
+
+        open("result/$molecule.dat", "a") do file
+            line = "================$molecule================\n"
             print(line)
             write(file, line)
             flush(file)
-    
-
-        end
-
-        println("Finished job")
-        
+            Threads.@threads for latvecs_point in latvecs_vals
+                atom_recipes.lattice_param = latvecs_point
+                crystal_structure = build(atom_recipes)
+                pspfiles = map(key -> joinpath(DIR_PSP_SCAN, ALL_SCAN_PSP[strip(key)]), atom_recipes.atom_names)
+                println(pspfiles)
+                ham = Hamiltonian(crystal_structure, pspfiles, ecutwfc, meshk=[10, 10, 10])
+                KS_solve_Emin_PCG!(ham, verbose=false,)
+                total_energy = sum(ham.energies)
+                line = "$latvecs_point\t\t$total_energy\n"
+                print(line)
+                write(file, line)
+                flush(file)
+            end
+            println("Finished job")
         end # open file
     end
 end
